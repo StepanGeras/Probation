c2a148d989222ec8c6481fe5b794a729dc84aba6
Shifernik <m.Disai@mail.ru>
2024-09-28 21:56:14 +0300

    Hibernate + criteria API (BookDao -> interface)

--- src/main/java/com/example/dao/BookDao.java
+++ src/main/java/com/example/dao/BookDao.java
@@ -1,68 +1,12 @@
 package com.example.dao;
 
 import com.example.entity.Book;
-import com.example.localization.Localization;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
-@Repository
-
-public class BookDao {
-
-    private final SessionFactory sessionFactory;
-
-    @Autowired
-    public BookDao(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-
-    @Transactional
-    public List<Book> getAllBooks() {
-        CriteriaBuilder criteriaBuilder = sessionFactory.getCriteriaBuilder();
-        CriteriaQuery<Book> criteriaQuery = criteriaBuilder.createQuery(Book.class);
-        Root<Book> root = criteriaQuery.from(Book.class);
-        criteriaQuery.select(root);
-
-        Query<Book> query = sessionFactory.getCurrentSession().createQuery(criteriaQuery);
-        return query.getResultList();
-    }
-
-    @Transactional
-    public void addBook(Book book) {
-        Session session = sessionFactory.getCurrentSession();
-        session.persist(book);
-    }
-
-    @Transactional
-    public boolean existsBookById(int id) {
-        Session session = sessionFactory.getCurrentSession();
-        Book book = session.get(Book.class, id);
-        return book != null;
-    }
-
-    @Transactional
-    public void updateBookById(int id, Book book) {
-        Session session = sessionFactory.getCurrentSession();
-        Book newBook = session.get(Book.class, id);
-        newBook.setAuthor(book.getAuthor());
-        newBook.setTitle(book.getTitle());
-        newBook.setDescription(book.getDescription());
-    }
-
-    @Transactional
-    public void deleteBook(int id) {
-        Session session = sessionFactory.getCurrentSession();
-        Book book = session.get(Book.class, id);
-        session.remove(book);
-    }
-
+public interface BookDao {
+    List<Book> getAllBooks();
+    void addBook(Book book);
+    boolean existsBookById(int id);
+    void updateBookById(int id, Book book);
+    void deleteBook(int id);
 }
--- src/main/java/com/example/service/BookService.java
+++ src/main/java/com/example/service/BookService.java
@@ -1,6 +1,6 @@
 package com.example.service;
 
-import com.example.dao.BookDao;
+import com.example.dao.BookDaoImpl;
 import com.example.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,31 +11,31 @@ import java.util.List;
 @Service
 public class BookService {
 
-    private final BookDao bookDao;
+    private final BookDaoImpl bookDaoImpl;
 
     @Autowired
-    public BookService(BookDao bookDao) {
-        this.bookDao = bookDao;
+    public BookService(BookDaoImpl bookDaoImpl) {
+        this.bookDaoImpl = bookDaoImpl;
     }
 
     public List<Book> readBooks() throws IOException {
-        return bookDao.getAllBooks();
+        return bookDaoImpl.getAllBooks();
     }
 
     public void createBook(Book book) throws IOException {
-        bookDao.addBook(book);
+        bookDaoImpl.addBook(book);
     }
 
     public void updateBook(int id, Book updatedBook) throws IOException {
-        bookDao.updateBookById(id, updatedBook);
+        bookDaoImpl.updateBookById(id, updatedBook);
     }
 
     public void deleteBook(int id) throws IOException {
-        bookDao.deleteBook(id);
+        bookDaoImpl.deleteBook(id);
     }
 
     public boolean existsBookById(int id) {
-        return !bookDao.existsBookById(id);
+        return !bookDaoImpl.existsBookById(id);
     }
 
 }
