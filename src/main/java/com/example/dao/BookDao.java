99e418f50f112593801aae21432008732a7767b7
Shifernik <m.Disai@mail.ru>
2024-09-28 21:48:33 +0300

    Hibernate + criteria API

--- src/main/java/com/example/dao/BookDao.java
+++ src/main/java/com/example/dao/BookDao.java
@@ -2,6 +2,9 @@ package com.example.dao;
 
 import com.example.entity.Book;
 import com.example.localization.Localization;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.hibernate.SessionFactory;
@@ -24,8 +27,12 @@ public class BookDao {
 
     @Transactional
     public List<Book> getAllBooks() {
-        Session session = sessionFactory.getCurrentSession();
-        Query<Book> query = session.createQuery("from Book", Book.class);
+        CriteriaBuilder criteriaBuilder = sessionFactory.getCriteriaBuilder();
+        CriteriaQuery<Book> criteriaQuery = criteriaBuilder.createQuery(Book.class);
+        Root<Book> root = criteriaQuery.from(Book.class);
+        criteriaQuery.select(root);
+
+        Query<Book> query = sessionFactory.getCurrentSession().createQuery(criteriaQuery);
         return query.getResultList();
     }
 
